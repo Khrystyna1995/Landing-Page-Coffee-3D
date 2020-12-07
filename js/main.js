c9478ed1f6c349bee82e28783c0134b0a291ed4c
Khrystyna1995 <kric29_95@i.ua>
2020-12-07 14:50:33 +0200

    finished

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-const navMenu = document.getElementById('nav-menu');
-    toggleMenu = document.getElementById('nav-toggle');
-    closeMenu = document.getElementById('nav-close');
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
 
-toggleMenu.addEventListener('click', event => {
-  navMenu.classList.toggle('show');
-});
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
 
-closeMenu.addEventListener('click', event => {
-    navMenu.classList.toggle('show');
-});
+// HIDDEN
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== MOUSEMOVE HOME IMG =====*/
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
 
